d8e5323a19c7d920cb25b4c05eeb5ae001715498
Paul <adebayotoheeb666@gmail.com>
2025-05-11 19:51:41 +0100

    coomit

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var cors = require('cors');
+const multer = require('multer');
 require('dotenv').config()
 
 var app = express();
+const upload = multer({ dest: 'uploads/' });
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,8 +13,17 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+app.post('/api/fileanalyse', upload.single('upfile'), function(req, res) {
+  if (!req.file) {
+    return res.json({ error: 'Please upload a file' });
+  }
+  
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
